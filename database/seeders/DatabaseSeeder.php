63932f44485696c14de366c53f53a8b1ae8aa524
mindfulcoder49 <alex.g.alcivar49@gmail.com>
2024-09-30 12:18:44 -0400

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Console\Commands\DownloadGrantsXML;  
+use App\Console\Commands\DownloadGrantsXML;  
 
 class DatabaseSeeder extends Seeder
 {
